22cf0df28ee9f0af22c869519b89c0d989db2ccb
gbaranski <root@gbaranski.com>
2021-05-27 19:38:23 +0200

    test: fix tests

--- src/lib.rs
+++ src/lib.rs
@@ -104,55 +104,72 @@ mod tests {
 
     #[test]
     fn test_hash() {
-        struct TestData<'a> {
-            message: &'static str,
-            digest: &'a [u16],
-        }
-        const DATA: &[TestData] = &[
-            TestData {
-                message: "",
-                digest: &[
-                    0xE225, 0x5BFB, 0xD3CF, 0x86E0, 0xDBE5, 0x2AA9, 0x6782, 0xEB8D,
+        [
+            (
+                "",
+                [
+                    0xE2, 0x25, 0x5B, 0xFB, 0xD3, 0xCF, 0x86, 0xE0, 0xDB, 0xE5, 0x2A, 0xA9, 0x67,
+                    0x82, 0xEB, 0x8D,
                 ],
-            },
-            TestData {
-                message: "AbCxYz",
-                digest: &[
-                    0x5A0F, 0xB1F1, 0xF014, 0x9827, 0xC536, 0x280F, 0xEAD1, 0x67D1,
+            ),
+            (
+                "AbCxYz",
+                [
+                    0x5A, 0x0F, 0xB1, 0xF1, 0xF0, 0x14, 0x98, 0x27, 0xC5, 0x36, 0x28, 0x0F, 0xEA,
+                    0xD1, 0x67, 0xD1,
                 ],
-            },
-            TestData {
-                message: "1234567890",
-                digest: &[
-                    0x3746, 0x689D, 0x2ED8, 0x0406, 0xEBE2, 0x038B, 0x5FDD, 0xF9D5,
+            ),
+            (
+                "1234567890",
+                [
+                    0x37, 0x46, 0x68, 0x9D, 0x2E, 0xD8, 0x04, 0x06, 0xEB, 0xE2, 0x03, 0x8B, 0x5F,
+                    0xDD, 0xF9, 0xD5,
                 ],
-            },
-            // TestData {
-            //     message: "Ala ma kota, kot ma ale.",
-            //     digest: &[
-            //         0xD662, 0xF8E0, 0x328D, 0x46CB, 0x53CC, 0xB89D, 0x219A, 0x9485,
-            //     ],
-            // },
-            // TestData {
-            //     message: "Ty, ktory wchodzisz, zegnaj sie z nadzieja.",
-            //     digest: &[
-            //         0xB534, 0xF7EF, 0xF714, 0x8C43, 0x2057, 0xDFD6, 0x1138, 0x7A30,
-            //     ],
-            // },
-            // TestData {
-            //     message: "Litwo, Ojczyzno moja! ty jestes jak zdrowie;",
-            //     digest: &[
-            //         0x7FEC, 0x8BC2, 0x482B, 0xA864, 0xCA69, 0x9270, 0x5207, 0x3CDD,
-            //     ],
-            // },
-        ];
-        for data in DATA {
-            let digest = hash(data.message.as_bytes());
-            assert_eq!(
-                digest, data.digest,
-                "invalid digest received for message: `{}`",
-                data.message
-            );
-        }
+            ),
+            (
+                "Ala ma kota, kot ma ale.",
+                [
+                    0xD6, 0x62, 0xF8, 0xE0, 0x32, 0x8D, 0x46, 0xCB, 0x53, 0xCC, 0xB8, 0x9D, 0x21,
+                    0x9A, 0x94, 0x85,
+                ],
+            ),
+            (
+                "Ty, ktory wchodzisz, zegnaj sie z nadzieja.",
+                [
+                    0xB5, 0x34, 0xF7, 0xEF, 0xF7, 0x14, 0x8C, 0x43, 0x20, 0x57, 0xDF, 0xD6, 0x11,
+                    0x38, 0x7A, 0x30,
+                ],
+            ),
+            (
+                "a".repeat(48000).as_str(),
+                [
+                    0x07, 0x2F, 0xB0, 0x3B, 0xC3, 0xC9, 0x96, 0x50, 0x66, 0x3B, 0x2B, 0x89, 0xA6,
+                    0xE9, 0x9F, 0x74,
+                ],
+            ),
+            (
+                "a".repeat(48479).as_str(),
+                [
+                    0xAA, 0x64, 0x8B, 0xAE, 0xF6, 0x95, 0x48, 0x33, 0xF9, 0x55, 0x5D, 0x55, 0xA7,
+                    0x97, 0xD2, 0xCB,
+                ],
+            ),
+            (
+                "a".repeat(48958).as_str(),
+                [
+                    0x9A, 0x9C, 0x15, 0x4F, 0x81, 0x7A, 0x48, 0xE4, 0xE2, 0x8D, 0x8A, 0x8C, 0x68,
+                    0x7A, 0xCD, 0x60,
+                ],
+            ),
+        ]
+        .iter()
+        .for_each(|(message, expected_hash)| {
+            let expected_hash = expected_hash
+                .chunks_exact(2)
+                .map(|v| u16::from_be_bytes([v[0], v[1]]))
+                .collect::<Vec<u16>>();
+            let hash = hash(message.as_bytes());
+            assert_eq!(hash, expected_hash.as_slice(), "for message {}", message,);
+        });
     }
 }
